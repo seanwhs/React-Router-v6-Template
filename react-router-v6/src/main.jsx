41887134a809d2ef1c44cd869bdd986e751578e1
seanwhs <seanwhs@gmail.com>
2024-02-21 22:51:27 +0800

    Added Contact Form

--- react-router-v6/src/main.jsx
+++ react-router-v6/src/main.jsx
@@ -6,6 +6,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import User from './components/User/User.jsx'
+import Contact from './components/Contact/Contact.jsx'
 import Github, {githubInfoLoader} from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
@@ -13,6 +14,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />}/>
       <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
       <Route path='user/' element={<User />} >
         <Route path=':userid' element={<User />} />
       </Route>
